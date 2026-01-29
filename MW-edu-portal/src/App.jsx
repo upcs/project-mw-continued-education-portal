e1792b4c823d424340f9fe256ec21ef8532aa230
Carter Chan <chanc28@up.edu>
2026-01-28 17:11:36 -0800

    I created the website and added some text onto it

---
+++ MW-edu-portal/src/App.jsx
@@ -0,0 +1,13 @@
+import SearchBar from './searchBar.jsx'
+import WelcomeMessage from './welcomeMessage.jsx'
+
+function App() {
+  return (
+    <>
+      <SearchBar/>
+      <WelcomeMessage/>
+    </>
+  );
+}
+
+export default App
---
+++ MW-edu-portal/src/welcomeMessage.jsx
@@ -0,0 +1,11 @@
+
+function WelcomeMessage() {
+    return (
+        <header>
+            <h1>Hello! Welcome to the MW Education Portal!</h1>
+            <p>Please! Take a look around!</p>
+        </header>
+    )
+}
+
+export default WelcomeMessage
