e84d7980c9f2380848b0c212075898249b06ab18
Hassinullah Niazy <niazy27@up.edu>
2026-02-19 10:29:36 -0800

    new commit

---
+++ MW-edu-portal/Modules/Header/header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>This is the Page Header</h1>
+    )
+}
+
+export default Header
--- MW-edu-portal/src/ModuleViewer.jsx
+++ MW-edu-portal/src/ModuleViewer.jsx
@@ -1,1 +1,12 @@
 
+import Logo from "../Modules/Logo/logo.jsx"
+import Button from "../Modules/Buttons/button.jsx"
+import Profile from "../Modules/ProfileView/profileView.jsx"
+
+function ModuleViewer() {
+    return (
+        <p>This is Module Viewer</p>
+    )
+}
+
+export default ModuleViewer
