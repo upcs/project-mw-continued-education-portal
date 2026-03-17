f1b350c141d6ce1aaa7766d78e6241c3c93bc267
QuincePham <phamt27@up.edu>
2026-03-17 15:48:58 +0000

    login working

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Dashboard from './pages/DashboardPage';
 import ProfilePage from './pages/ProfilePage';
 import SettingsPage from './pages/SettingsPage';
 import Login from './pages/login';
-import Signin from './pages/signin';
+import Signup from './pages/signup';
 import Profile from './pages/profile';
 
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
@@ -30,7 +30,7 @@ function App()
           <Route path="/profile" element={<ProfilePage/>}/>
           <Route path="/settings" element={<SettingsPage/>}/>
           <Route path="/login" element={<Login/>}/>
-          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/signup" element={<Signup/>}/>
 	  <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </Router> 
--- server/server2.js
+++ server/server2.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve React static files
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -14,13 +16,37 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-app.post('/server', (req, res) => {
-	var my_response = {message : "hello"}
-	var query = "select * from userdata"
+app.post('/login', (req, res) => {
+	//console.log(req.body);
+	var body = req.body;
+	var pass = body.password;
+	var user = body.username;
+	var query = "select * from userdata where username =\"" + user + "\" and password = \"" + pass + "\";"
 	dbms.dbquery(query, (err, response) => {
-		res.json(response[0].username);
+	var test = {success : true};
+	console.log(test.success);
+		if (response[0] != undefined) {res.json({success: true});}
+		else {res.json({success: false});}
 	});
 		});
+app.post('/signup', (req, res)=> {
+	var body = req.body;
+	var name = body.name;
+	var email = body.email;
+	var whatsapp = body.whatsapp;
+	var photo = body.photo;
+	var username = body.username;
+	var password = body.password;
+
+        var query = "Select from userdata where username = \"" + username + "\";"
+	var query2 = "intert into userdata values (" //TODO
+	dbms.dbquery(query, (err, reponse)=> {
+	//error, username already exists
+	if (reponse[0] != undefined) {res.json({error: "username"});}
+	
+	});
+
+	});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
